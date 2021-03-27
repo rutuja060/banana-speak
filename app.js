e227b10e260e1f55e7d6d5dca7af1ec5cc852af7
rutuja060 <kadamrutuja103@gmail.com>
2021-03-27 23:16:42 +0530

    commit:styling

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input")
 var outputDiv=document.querySelector("#output")
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+
+
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+
+
+function errorHandler(error){
+    console.log("error occured",error);
+}
+
 
 
 function clickHandler(){
-    outputDiv.innerText="dnndn " + txtInput.value;
+    //outputDiv.innerText="dnndn " + txtInput.value;
+
+var inputText=txtInput.value;
+
+//calling server for processing
+fetch(getTranslationURL(inputText))
+.then(response=> response.json())
+.then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerHTML=translatedText;
+})
+    //console.log(json.contents.translated))
+.catch(errorHandler)
+
+
 };
 
 
